ef3d4234c5bb6c2357abaa0a83056c3fed5859a0
MBleeker <maurits-bleeker@hotmail.com>
2016-02-25 15:21:25 +0100

    first version part 1

--- empty_function.m
+++ empty_function.m
@@ -1,7 +1,23 @@
-function [ output_args ] = untitled( input_args )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [] = harris_corner_detector( image_path, sgima )
+
+kernel_lenght = (6 * sigma) + 1;
+G = fspecial('guassian',[1 kernel_lenght],sigma);
 
 
+im = imread(image_path);
+im = im2double(im);
+im = gb2grey(im);
+
+
+
+Ix = gaussuasnDer(im, G, sigma);
+Iy = gaussuasnDer(im, G', sigma)
+
+A = Ix.^2;
+C = Iy.^2;
+B = Ix .* Iy; 
+
+H = (A.*C - B .^2) - 0.04 (A .+ B).^2
+imshow(H);
 end
 
