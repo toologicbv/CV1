c9dfb242aeeff7aaa0e3af3d830ccaf577a16d18
MBleeker <maurits.bleeker@nubisonline.nl>
2016-03-11 08:53:35 +0100

    tried a lot, no results

--- Assignment 4/image_stitching.m
+++ Assignment 4/image_stitching.m
@@ -24,31 +24,36 @@ function [ output_args ] = image_stitching( img_path1, img_path2 )
         I1 = im2single(I1_255);
         I2 = im2single(I2_255);
     end
+    
+    size(I1)
+    size(I2)
 
-    N = 200;
+    % TODO: zorg er voor dat tform alleen gebruikte wordt als er genoeg
+    % inliers zijn, anders werkt het niet goed. 
+    %N = 200;
     %tform = RANSACv1(img_path1, img_path2, N, true)
+    [f1, d1] = vl_sift(I1);
+    [f2, d2] = vl_sift(I2);
+    % find matches between the descriptors
+    
+    % DIT IS HARDCODED FOR NOW
     tform = [0.9878,   -0.0881, -204.9841;
              0.0880,    0.9960,  -55.9973;
              0,         0,    1.0000];
     tform(1:2,1:2) = tform(1:2,1:2)';
-    
     x =  round(abs(tform(1,3)),0);
-    round(abs(tform(2,3)),0)
-    y =  round(abs(tform(2,3)),0) - 41;
-    
+    y =  round(abs(tform(2,3)),0); 
     
     tform = affine2d(tform');
-    size(I2)
     I2 = imwarp(I2,tform);
-    size(I2)
     
-    x_length= max(round(x,0) + size(I1,1), size(I1,1));
-
-    y_length = max(round(y,0) + size(I1,2), size(I1,2));
+    x_length = max(round(x,0) + size(I1,1), size(I1,1))
+    y_length = max(round(y,0) + size(I1,2), size(I1,2))
 
     % Create new image
-    newimage = zeros(x_length, y_length);
-    newimage(y:size(I2,1)-1+y, x:size(I2,2)-1+x) = I2;
+    newimage = zeros(y_length, x_length);
+    
+    newimage(15:(size(I2,1))+14, x:size(I2,2)-1+x) = I2;
     newimage(1:size(I1,1), 1:size(I1,2)) = I1;
     
     figure, imshow(newimage);   
